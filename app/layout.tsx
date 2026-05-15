023e4f1f4a97755dfbfce76f3896f5037d7e64c2
Bojan Ratej <bojanratej1964@gmail.com>
2026-05-15 14:19:02 +0200

    Scroll to top on page navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import ScrollToTop from "@/components/ScrollToTop";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +28,8 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ScrollToTop />{children}</body>
     </html>
   );
 }
